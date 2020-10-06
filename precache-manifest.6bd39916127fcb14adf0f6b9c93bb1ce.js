c113d431e54d8c97c69fa25be0de771bc0ce041c
qudratgulamov <qudratgulamov6465@gmail.com>
2020-10-06 19:20:56 +0500

    Updates

---
+++ precache-manifest.6bd39916127fcb14adf0f6b9c93bb1ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d09d476271a313b2afec0af7a956ab50",
+    "url": "/React-todoList/index.html"
+  },
+  {
+    "revision": "246b814569317091a335",
+    "url": "/React-todoList/static/css/main.5a88ffd6.chunk.css"
+  },
+  {
+    "revision": "69149ebcdaf75473e8f3",
+    "url": "/React-todoList/static/js/2.a69ac7f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todoList/static/js/2.a69ac7f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "246b814569317091a335",
+    "url": "/React-todoList/static/js/main.a6d6079b.chunk.js"
+  },
+  {
+    "revision": "a855710d47e890405512",
+    "url": "/React-todoList/static/js/runtime-main.47550b63.js"
+  }
+]);
